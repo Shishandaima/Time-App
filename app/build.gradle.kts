1466d8464c0bf7ac2c7cb5467271bf7da79b58b9
Shishandaima <ppp7714@126.com>
2026-06-22 18:12:10 +0800

    将APK版本号改为0.5.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.timemaster"
         minSdk = 26
         targetSdk = 35
-        versionCode = 8
-        versionName = "0.5.1"
+        versionCode = 9
+        versionName = "0.5.2"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
